fd87ed1c0e17f80381c6f93a16ef13dfa9b13c93
Justin Trotter <trotter.justin@gmail.com>
2020-02-26 02:29:20 -0600

    home is roughly done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-          Justin Trotter's Website
-        </h1>
-        <h3>
-          is under construction.
-        </h3>
-      </header>
+      <Header/>
     </div>
   );
 }
