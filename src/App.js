d795a24eddf25928ed220d566785d092decc17d5
Justin Trotter <trotter.justin@gmail.com>
2020-02-27 01:57:12 -0600

    added skills

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
+import Experience from './Components/Experience';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <About/>
+      <Experience/>
     </div>
   );
 }
