ebbeea9a34fc89cb7517830a8549bba04fc48f97
Justin Trotter <trotter.justin@gmail.com>
2020-03-11 19:46:23 -0500

    trying to get images to work

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,18 +9,19 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/images/`,
-      },
-    },
+    `gatsby-plugin-sharp`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
